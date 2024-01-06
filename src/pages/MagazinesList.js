4e758ad5adc540253d926b57417c16662f132092
vinod4848 <vinodku4848@gmail.com>
2024-01-06 12:48:54 +0530

    delete pfd Magazine

--- src/pages/MagazinesList.js
+++ src/pages/MagazinesList.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { base_url } from "../utils/base_url";
+import { Modal, Button } from "react-bootstrap";
 
 const MagazinesList = () => {
   const [magazines, setMagazines] = useState([]);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [deleteMagazineId, setDeleteMagazineId] = useState(null);
 
   useEffect(() => {
     const fetchMagazines = async () => {
@@ -26,6 +29,29 @@ const MagazinesList = () => {
     link.click();
   };
 
+  const handleDelete = (id) => {
+    setDeleteMagazineId(id);
+    setShowDeleteModal(true);
+  };
+
+  const confirmDelete = async () => {
+    try {
+      await axios.delete(`${base_url}/magazines/${deleteMagazineId}`);
+      // Update the state after successful deletion
+      setMagazines((prevMagazines) =>
+        prevMagazines.filter((magazine) => magazine._id !== deleteMagazineId)
+      );
+    } catch (error) {
+      console.error("Error deleting magazine:", error);
+    } finally {
+      setShowDeleteModal(false);
+    }
+  };
+
+  const cancelDelete = () => {
+    setShowDeleteModal(false);
+  };
+
   return (
     <div className="container mt-5">
       <h1>Magazines List</h1>
@@ -39,10 +65,18 @@ const MagazinesList = () => {
               </div>
               <div style={{ marginLeft: "auto" }}>
                 <button
-                  className="btn btn-primary"
+                  className="btn btn-primary mr-2"
+                  style={{ marginRight: "7px" }}
                   onClick={() => downloadPdf(magazine.image)}
                 >
                   Download PDF
+                </button>
+
+                <button
+                  className="btn btn-danger"
+                  onClick={() => handleDelete(magazine._id)}
+                >
+                  Delete
                 </button>
               </div>
             </div>
@@ -50,6 +84,20 @@ const MagazinesList = () => {
           </li>
         ))}
       </ul>
+      <Modal show={showDeleteModal} onHide={cancelDelete}>
+        <Modal.Header closeButton>
+          <Modal.Title>Delete Magazine</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Are you sure you want to delete this magazine?</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={cancelDelete}>
+            Cancel
+          </Button>
+          <Button variant="danger" onClick={confirmDelete}>
+            Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
